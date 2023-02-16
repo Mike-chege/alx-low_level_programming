4bdf266dd893c57aca5b1d3b68d47b3c97d9d5be
mikechege01 <mike@mail.com>
2023-02-16 12:06:13 -0800

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly
+ * main - prints exactly 
  * and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-		write(1, "and that piece of art is useful\" - Dora Korpar, "\
-					    "2015-10-19 \n", 59);
- 	          		return (1);
+		write(2, "and that piece of art is useful\" - Dora Korpar,"\ 
+				"2015-10-19\n", 59);
+			return (1);
 }
