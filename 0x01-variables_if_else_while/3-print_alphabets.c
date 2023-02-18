6118b9cb314bfcccb7ce4e43db1b679aa63ee2b7
mikechege01 <mike@mail.com>
2023-02-18 04:52:29 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+
+/**
+ * main - prints the alphabet in lowercse, and then uppercse,
+ * followed by a new line
+ * return: Always 0 (Succss)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
