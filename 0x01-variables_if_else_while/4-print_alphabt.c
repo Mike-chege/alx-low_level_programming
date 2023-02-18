a43ac1ff672ee9f8c600484a0d3c7f957d9d08c0
mikechege01 <mike@mail.com>
2023-02-18 05:38:13 -0800

    4-print_alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the alphabet in lowercase,
- * folloed by a new line, except q and e 
+ * followed by a new line, except q and e 
  * Return: Always 0 (Success)
  */
 int main(void)
