8378960d7015aaf33387f33acfdb5acb91966c69
mikechege01 <mike@mail.com>
2023-04-05 14:04:28 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,8 +3,7 @@
 #include "lists.h"
 
 /**
- * reverse_listint - function that reverses
- * a listint_t linked list
+ * reverse_listint - function that reverses a listint_t linked list
  * @head: head of linked list
  *
  * Return: node of the reversed list
