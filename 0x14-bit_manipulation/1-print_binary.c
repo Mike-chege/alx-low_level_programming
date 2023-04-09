48dade3ab632524e542b28d7d1ed2d319f90b311
mikechege01 <mike@mail.com>
2023-04-09 13:08:49 -0700

    print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,14 +3,21 @@
 
 /**
  * print_binary - Prints the binary representation of a number.
+ *
  * @n: The number to be printed in binary.
+ *
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n == 0)
 	{
-		_putchar(n >> 1);
+		_putchar('0');
+		return;
 	}
-
-	_putchar((n & 1) + '0');
+	
+	for (int i = 63; i >= 0; i--)
+	{
+		int bit = (n >> i) & 1;
+		_putchar(bit + '0');
+	}
 }
