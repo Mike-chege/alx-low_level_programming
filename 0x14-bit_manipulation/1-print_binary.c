b9e0af345291d7c6f0b87b364253b82a00b32351
mikechege01 <mike@mail.com>
2023-04-09 13:23:35 -0700

    print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,13 +12,12 @@ void print_binary(unsigned long int n)
 	unsigned long int num = 1;
 	int i, bit;
 
-	for (i = 0; i < 63; i++)
+	for (i = 0; i < 64; i++)
 	{
 		bit = (n & (num << i)) ? 1 : 0;
-		if (bit || i == 63)
+		if (bit || i == 64)
 		{
 			_putchar(bit + '0');
 		}
 	}
 }
-
