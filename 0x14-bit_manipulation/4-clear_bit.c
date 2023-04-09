69943a5117b72e2dacd8c821c93030cab4c69f6c
mikechege01 <mike@mail.com>
2023-04-09 08:59:35 -0700

    4clearbit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,9 +1,9 @@
 #include"main.h"
 
 /**
- * clear_bit - sets the value of a bit to 0 at a given index
- * @n: pointer to the bit
- * @index: The index position to change, starting from 0
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: pointer to the bit.
+ * @index: The index position to change, starting from 0.
  *
  * Return: -1 if an error occurs or 1 if successful.
  */
