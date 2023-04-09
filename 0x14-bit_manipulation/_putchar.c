9d5063afbcad22b74224849d4d87a2b26e5cc62c
mikechege01 <mike@mail.com>
2023-04-09 10:05:09 -0700

    _putchar.c added

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,4 +1,14 @@
 #include"main.h"
 #include<unistd.h>
 
+/**
+ * _putchar - writes a character to standard output.
+ * @c: is the character to be printed.
+ *
+ * Return: 1 on success, on error return -1 and set errno appropriately.
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
 
