cc14e587380c403fdb9c511e78bd10b801cc10a4
mikechege01 <mike@mail.com>
2023-04-10 16:22:16 -0700

    2-append_txt.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,8 +1,8 @@
-#include"main.h"
-#include<fnctl.h>
+#include "main.h"
+#include <fcntl.h>
 
 /**
- * int append_text_to_file - appends text at the end of a file.
+ * append_text_to_file - appends text at the end of a file.
  * @filename: is the name of the file.
  * @text_content: is the NULL terminated string.
  *
@@ -10,33 +10,31 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int open_byts, write_byts, num = 0; /*declaring variables*/
+        int open_byts, write_byts, num = 0;
 
-	if (filename == NULL)
-		return (-1);
-	
-	if (text_content != NULL)
-	{
-		for (num = 0; text_content[num]; num++)
-		{
-			/* nothing done here*/
-		}
-			
-	}
-	
-	open_byts = open(filename, O_WRONLY | O_APPEND);
-	if (open_byts == -1)
-		return(-1);
+        if (filename == NULL)
+                return (-1);
 
-	write_byts = write(open_byts, text_content, num);
-	if (write_byts == -1)
-	{
-		close(open_byts);
-		return (-1);
-	}
+        if (text_content != NULL)
+        {
+                for (num = 0; text_content[num]; num++)
+                {
+                        /* nothing done here*/
+                }
+        }
 
-	close(open_byts);
+        open_byts = open(filename, O_WRONLY | O_APPEND);
+        if (open_byts == -1)
+                return (-1);
 
-	return (1);
+        write_byts = write(open_byts, text_content, num);
+        if (write_byts == -1)
+        {
+                close(open_byts);
+                return (-1);
+        }
+
+        close(open_byts);
+
+        return (1);
 }
-
