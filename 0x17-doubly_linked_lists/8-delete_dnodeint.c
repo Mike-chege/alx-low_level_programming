2656206ffbf3181206eaa7726d2a363635c89cd0
mikechege01 <mike@mail.com>
2023-05-19 02:44:52 -0700

    Task[8-delete_dnodeint.c] fixed betty/assignment operator

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index 
+ * delete_dnodeint_at_index - deletes the node at index
  * of a dlistint_t linked list
  * @head: pointer to a pointer to the head of the list
  * @index: index of the node that should be deleted
- *
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
@@ -30,7 +29,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	for (i = 0; i < index && temp != NULL; i++)
 		temp = temp->next;
 
-	if (i != index || temp = NULL)
+	if (i != index || temp == NULL)
 		return (-1);
 
 	temp->prev->next = temp->next;
