5fb48fe205f8a097d7f520f9c3d4e2ffd539ccb7
mikechege01 <mike@mail.com>
2023-05-19 00:59:43 -0700

    Task[0-print_dlistint.c] fixed the while statement

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,14 @@
  */
 ssize_t print_dlistint(const dlistint_t *h)
 {
+	const dlistint_t *i;
 	size_t count = 0;
 
-	while (h != NULL)
+	i = h;
+	while (i)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", i->n);
+		i = i->next;
 		count++;
 	}
 	return (count);
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef __LISTS_H__
 #define __LISTS_H__
 
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * struct dlistint_s - doubly linked list
