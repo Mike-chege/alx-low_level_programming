7f89f30b37d0fe8cd68fb34cf75377b07d8001f3
mikechege01 <mike@mail.com>
2023-05-22 09:52:29 -0700

    dynamic library files[added permission to some unpermitted files

--- 0x18-dynamic_libraries/compare.c
+++ 0x18-dynamic_libraries/compare.c
@@ -1,12 +1,8 @@
 /**
  * _strcmp - compares two strings
- *
  * @s1: first string
  * @s2: second string
- *
  * Return: -15 if s1 < s2
- *          0  if s1 = s2
- *          15 if s1 > s2
  */
 int _strcmp(char *s1, char *s2)
 {
