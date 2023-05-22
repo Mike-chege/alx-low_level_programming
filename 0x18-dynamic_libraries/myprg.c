9e71ec9e2ca38cd5f0a4ca4b93f4d763c6684779
mikechege01 <mike@mail.com>
2023-05-22 09:03:14 -0700

    Task [few]

---
+++ 0x18-dynamic_libraries/myprg.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - check code
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("Hello, world!"));
+	return (0);
+}
