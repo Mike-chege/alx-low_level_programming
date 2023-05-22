b1bbf141b100310bb6fefe3caac49fc0783bdeda
mikechege01 <mike@mail.com>
2023-05-22 10:30:47 -0700

    Task[1-create_dynamic.c] creates a script called liball.so

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -2,7 +2,6 @@
 # compile all .c files in the current directory
 gcc -c -Wall -Werror -fpic *.c
 # create the dynamic library from object files
-gcc - shared -o liball.so *.o
+gcc -shared -o liball.so *.o
 # cleanup the object files
 rm *.o
-
