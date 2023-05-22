998ad4e6ef0462d42b1f7fde16cb6ee8de9f6593
mikechege01 <mike@mail.com>
2023-05-22 11:27:08 -0700

    code injection for dynamic library to use on a shell script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libinjection.so https://github.com/mikechege01/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libinjection.so
-export LD_PRELOAD=/tmp/libinjection.so
+wget -O /tmp/injection.so https://github.com/mikechege01/alx-low_level_programming/raw/master/0x18-dynamic_libraries/injection.so
+export LD_PRELOAD=/tmp/injection.so
