7f5e18c007c387c0437a9ff80274128122cfc6ab
Mike-chege <mikechege93@gmail.com>
2023-07-26 06:37:07 -0700

    Fixed typos / added documentation / removed exceptions

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,39 +1,25 @@
 #!/usr/bin/python3
 """
-module for interview prep, tried more efficient solution but too much
+Returns the perimeter of
+An island described in a grid
 """
 
 
 def island_perimeter(grid):
-    """ gets perimeter of island represented as grid
     """
-    if not isinstance(grid, list):
-        return 0
-    island_p = 0
-    for row_i, row in enumerate(grid):
-        if not isinstance(row, list):
-            return 0
-        for col_i, block in enumerate(row):
-            if type(block) is not int:
-                return 0
-            if block == 1:
-                island_p += get_block_sum(grid, row_i, col_i)
-    return island_p
-
-
-def get_block_sum(grid, r_i, c_i):
+    Returns the perimeter of the island
     """
-    gets the perimeter of a block
-    """
-    next_block = None
-    block_p = 4
-    try:
-        r_block = grid[r_i][c_i + 1]
-        l_block = grid[r_i][c_i - 1]
-        t_block = grid[r_i - 1][c_i]
-        b_block = grid[r_i + 1][c_i]
-    except:
-        raise ValueError("ISLAND IS NOT SURROUNDED BY WATER")
+    row = len(grid[0])
+    column = len(grid)
+    edges = 0
+    perimeter = 0
 
-    block_p += -1 * (r_block + l_block + t_block + b_block)
-    return block_p
+    for i in range(column):
+        for j in range(row):
+            if grid[i][j] == 1:
+                perimeter += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return perimeter * 4 - edges * 2
